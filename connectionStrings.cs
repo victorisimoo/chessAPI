a38a03126111df13ebc4d27bb4fde7ee9f002875
Jossue Samayoa <josue.samayoa@gmail.com>
2023-01-28 01:18:11 -0600

    connectionString from appSettings

--- connectionStrings.cs
+++ connectionStrings.cs
@@ -4,5 +4,5 @@ public sealed class connectionStrings
         /// <summary>
         /// Cadena de conexión a base de datos relacional
         /// </summary>
-        public string? relationalDBConn { get; set; } = "Server=localhost;Port=5432;Database=chessDB;User Id=postgres;Password=misDatos2023!;Pooling=true;MinPoolSize=3;MaxPoolSize=20;Max Auto Prepare=15;Enlist=false;Auto Prepare Min Usages=3";
+        public string relationalDBConn { get; set; }
     }
